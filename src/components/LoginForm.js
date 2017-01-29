0fd1e66ba019c04a4985b68f63922ed1ef9a394a
Gery Egan <gery.egan@gmail.com>
2017-01-29 15:11:26 -0800

    LoginForm boiler basic set up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
             <Card>
-                <Text>
-                    OYOYOY
-                </Text>
+                <CardSection>
+                    <Input label='Email' placeholder='email@gmail.com' />
+                </CardSection>
+
+                <CardSection>
+                    <Input label='Password' placeholder='password' />
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
             </Card>
         );
     }
