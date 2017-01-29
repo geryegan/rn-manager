32e18264a3b1f269a11c4e99638c832776380160
Gery Egan <gery.egan@gmail.com>
2017-01-28 22:56:52 -0800

    firebase config

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDCnU3E2xt_8M1kd87wmcqM0DJXObtXs4w',
+            authDomain: 'employee-manager-bc193.firebaseapp.com',
+            databaseURL: 'https://employee-manager-bc193.firebaseio.com',
+            storageBucket: 'employee-manager-bc193.appspot.com',
+            messagingSenderId: '617202847716'
+        };
+        firebase.initializeApp(config);
+    }
     render() {
         return (
             <Provider store={createStore(reducers)}>
