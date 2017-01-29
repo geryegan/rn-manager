d0a8cc0f8aa40bb9b98f8de02fe04f10242fb5ea
Gery Egan <gery.egan@gmail.com>
2017-01-29 14:27:14 -0800

    LoginForm component added

---
+++ src/components/LoginForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Card } from './common';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <Card>
+                <Text>
+                    OYOYOY
+                </Text>
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
