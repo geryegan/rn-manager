6e622677f1ced95020103286083ef2d092cc15bc
Gery Egan <gery.egan@gmail.com>
2017-01-31 18:51:21 -0800

    redux thunk wired up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
@@ -17,8 +18,9 @@ class App extends Component {
         firebase.initializeApp(config);
     }
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
-            <Provider store={createStore(reducers)}>
+            <Provider store={store}>
                 <LoginForm />
             </Provider>
         );
