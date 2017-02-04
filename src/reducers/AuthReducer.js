458ccea83314fe47bd6988eff2716bfbc4e4a198
Gery Egan <gery.egan@gmail.com>
2017-02-03 22:03:01 -0800

    Auth status spinner logic

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,25 +1,30 @@
 import { EMAIL_CHANGED,
     PASSWORD_CHANGED,
     LOGIN_USER_SUCCESS,
-    LOGIN_USER_FAIL
+    LOGIN_USER_FAIL,
+    LOGIN_USER
  } from '../actions/types';
 
 const INITIAL_STATE = { 
     email: '',
-    password: ''
+    password: '',
+    error: '',
+    loading: false
  };
 
 export default (state = INITIAL_STATE, action) => {
     console.log('ACTION FROM REDUCER', action);
     switch (action.type) {
+        case LOGIN_USER:
+            return { ...state, loading: true, error: null };
         case EMAIL_CHANGED: 
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED: 
             return { ...state, password: action.payload };
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload, error: null };
+            return { ...state, user: action.payload, error: null, loading: false };
         case LOGIN_USER_FAIL:
-            return { ...state, error: 'Authentication failed' };
+            return { ...state, error: 'Authentication failed', loading: false, password: '' };
         default: return state;
     }
 };
