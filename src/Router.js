042ee962621847f23d0f2a9d81cf8ff83b8cd653
Gery Egan <gery.egan@gmail.com>
2017-02-27 18:16:53 -0800

    added nav button create employee

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import CreateEmployee from './components/CreateEmployee';
 
 const RouterComponent = () => {
   return (
-    <Router sceneStyle={{ paddingTop: 55 }}>
+    <Router navigationBarStyle={{ backgroundColor: '#FFF' }} sceneStyle={{ paddingTop: 55 }}>
         <Scene key='auth'>
             <Scene key="login" component={LoginForm} title="Login" />
         </Scene>
 
         <Scene key='main'>
-            <Scene key="employeeList" component={EmployeeList} title="Employees" />
+            <Scene 
+            key="employeeList" 
+            component={EmployeeList} 
+            title="Employees" 
+            rightTitle='+' 
+            onRight={() => { Actions.createEmployee(); }}
+            />
+            <Scene 
+            key="createEmployee" 
+            component={CreateEmployee} 
+            title="Create Employee" 
+            />
         </Scene>
 
     </Router>
