1562036c19b7b9d3aa23c65a55ebce5a85975907
Gery Egan <gery.egan@gmail.com>
2017-03-04 15:11:24 -0800

    fetch employee data from firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE } from './types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
@@ -12,9 +12,27 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
     const { currentUser } = firebase.auth();
     //redux thunk werkaround
-    return () => {
+    return (dispatch) => {
         firebase.database().ref(`users/${currentUser.uid}/employees`)
          .push({ name, phone, shift })
-          .then(() => Actions.employeeList({ type: 'reset' }));
+          .then(() => {
+              dispatch({ type: EMPLOYEE_CREATE });
+              Actions.employeeList({ type: 'reset' });
+            }
+          );
+    };
+};
+
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+    
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+         .on('value', snapshot => {
+             dispatch({
+                 type: EMPLOYEES_FETCH_SUCCESS,
+                 payload: snapshot.val()
+             });
+         });
     };
 };
