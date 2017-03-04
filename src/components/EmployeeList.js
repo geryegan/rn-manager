790892e2aa979486e27cde587a1a02f7eabc518a
Gery Egan <gery.egan@gmail.com>
2017-03-04 15:12:22 -0800

    data from firebase to array for ListView

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { View, Text, ListView } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+    componentWillMount() {
+        this.props.employeesFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        //nextProps are the next set of props that this component will be rendered cloneWithRows
+        //this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+
     render() {
+        console.log(this.props);
         return (
             <View>
                 <Text>Employee List</Text>
@@ -18,4 +40,11 @@ class EmployeeList extends Component {
     }
 }
 
-export default EmployeeList;
+const mapStateToProps = (state) => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+    });
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
