f8285c4388da1c1ed2812570b84a0c8328b0ea0d
Gery Egan <gery.egan@gmail.com>
2017-03-07 14:37:29 -0800

    employee list from firebase store

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
-import { View, Text, ListView } from 'react-native';
+import { ListView } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
     componentWillMount() {
@@ -22,20 +23,17 @@ class EmployeeList extends Component {
 
         this.dataSource = ds.cloneWithRows(employees);
     }
-
+    renderRow(employee) {
+        return <ListItem employee={employee} />;
+    }
     render() {
         console.log(this.props);
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <ListView 
+            enableEmptySections
+            dataSource={this.dataSource}
+            renderRow={this.renderRow}
+            />
         );
     }
 }
